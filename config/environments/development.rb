ab7f596771650fee1203fe25cec8bdd6ad58d806
niranjandasMM <niranjandasmm@gmail.com>
2023-05-29 18:37:55 +0530

    Classified

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,8 +64,8 @@ Rails.application.configure do
     address: "smtp.gmail.com",
     port: 587,
     domain: "gmail.com",
-    user_name: "niranjandasmm@gmail.com",
-    password: "gehwfqggetussjjb",
+    user_name: "<example@gmail.com>",
+    password: "<app_password>",
     authentication: "plain",
     enable_starttls_auto: true
   }
